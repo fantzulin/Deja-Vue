cd332bb1336d82f72ea13e0803c1c6a1fa62045a
Chris.Fan <fanchi0917@gmail.com>
2023-05-02 22:23:38 +0800

    update: transition tag effect

---
+++ vueture/src/components/Test.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <button @click="isShow = !isShow">show / hide</button>
+        <transition name="hello" appear>
+            <h1 v-show="isShow">Hello!</h1>
+        </transition>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Test',
+    data() {
+        return {
+            isShow: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+    h1 {
+        background-color: lightgray;
+    }
+
+    .hello-enter-active {
+        animation: title 1s;
+    }
+
+    .hello-leave-active {
+        animation: title 1s reverse;
+    }
+
+    @keyframes title {
+        from {
+            transform: translateX(-100%);
+        }
+        to {
+            transform: translateX(0px);
+        }
+    }
+</style>
