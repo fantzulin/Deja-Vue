652be3185d0d3b1d81b9fa91aaee003eca084f5c
Chris.Fan <fanchi0917@gmail.com>
2023-05-04 12:34:55 +0800

    update: 將配置代理服務器整理成資料

---
+++ vueture/19_src_配置代理服務器/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button @click="getAbout">get 5000 msg</button>
+    <button @click="getEmail">get 5001 msg</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'App',
+  methods: {
+    getAbout() {
+      axios.get('http://localhost:8080/chris/about').then(
+        response => {
+          console.log('請求成功了', response.data)
+        },
+        error => {
+          console.log('請求失敗了', error.message)
+        }
+      )
+    },
+    getEmail() {
+      axios.get('http://localhost:8080/demo/email').then(
+        response => {
+          console.log('請求成功了', response.data)
+        },
+        error => {
+          console.log('請求失敗了', error.message)
+        }
+      )
+    }
+  }
+}
+</script>
