1488078202720c1bb85565727125349bfd5c7992
Chris.Fan <fanchi0917@gmail.com>
2023-05-05 22:05:54 +0800

    update: Vuex介紹, 先用純 vue 寫案例

---
+++ vueture/23_src_count_pure_vue/components/Count.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <h1>當前求和為: {{ sum }}</h1>
+        <select v-model.number="n">
+            <option :value="1">1</option>
+            <option :value="2">2</option>
+            <option :value="3">3</option>
+        </select>
+        <button @click="add">+</button>
+        <button @click="minus">-</button>
+        <button @click="addOdd">當前求和為奇數再加</button>
+        <button @click="addWait">wait second</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Count',
+    data() {
+        return {
+            n: 1, // 用戶選擇的數字
+            sum: 0 // 當前的和
+        }
+    },
+    methods: {
+        add () {
+            this.sum += this.n
+        },
+        minus () {
+            this.sum -= this.n
+        },
+        addOdd () {
+            if(this.sum % 2) {
+                this.sum += this.n
+            }
+        },
+        addWait () {
+            setTimeout(() => {
+                this.sum += this.n
+            }, 1000)
+        }
+    }
+}
+</script>
+
+<style scoped>
+button {
+    margin-left: 10px;
+}
+</style>
