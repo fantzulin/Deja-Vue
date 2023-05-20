2c8323b5a85857aa94d4ce141d3d2513c4c505c9
Chris.Fan <fanchi0917@gmail.com>
2023-05-20 14:14:03 +0800

    update: 路由基本使用

--- vueture/src/App.vue
+++ vueture/src/App.vue
@@ -1,29 +1,34 @@
 <template>
   <div>
-    <Count/>
-    <hr>
-    <Person/>
+    <div class="row">
+      <div class="col-12">
+        <div class="page-header">
+          <h1>Vue Router Demo</h1>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-3">
+        <div class="list-group">
+          <!-- Vue 中借助 router-link 標籤實現路由的切換 -->
+          <router-link class="list-group-item list-group-item-action" active-class="active" to="/about">About</router-link>
+          <router-link class="list-group-item list-group-item-action" active-class="active" to="/home">Home</router-link>
+        </div>
+      </div>
+      <div class="col-9">
+        <div class="panel">
+          <div class="panel-body">
+            <!-- 指定組件的呈現位置 -->
+            <router-view></router-view>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Count from './components/Count'
-import Person from './components/Person'
 export default {
   name: 'App',
-  components: {
-    Count,
-    Person
-  },
-
-  mounted() {
-    // console.log('App', this)
-  }
 }
 </script>
-
-<style>
-h4 {
-    color: blue;
-}
-</style>
--- vueture/src/main.js
+++ vueture/src/main.js
@@ -2,21 +2,19 @@
 import Vue from 'vue'
 // 引入 App
 import App from './App.vue'
-// 引入插件
-import VueResource from 'vue-resource'
-// 引入 store
-import store from './store'
+// 引入
+import VueRouter from 'vue-router'
+// 引入路由器
+import router from './router'
+
 // 關閉 Vue 生產提示
 Vue.config.productionTip = false
-// 使用插件
-Vue.use(VueResource)
+// 應用插件
+Vue.use(VueRouter)
 
 // 創建 vm
 new Vue({
   el: '#app',
   render: h => h(App),
-  store,
-  beforeCreate() {
-    Vue.prototype.$bus = this
-  }
+  router: router
 })
