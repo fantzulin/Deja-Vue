819dc0dcdae729dcdae6ae20e631d5a134b8a76e
Chris.Fan <fanchi0917@gmail.com>
2023-05-20 16:39:07 +0800

    update: 多級路由練習

---
+++ vueture/31_src_多級路由/pages/Message.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <ul>
+            <li>
+                <a href="/">Home 組件的 message</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Message',
+}
+</script>
+
+<style>
+
+</style>
